64b5c67f4de60a953560c02696b5f22695fda518
umikaze-games <c463384962@gmail.com>
2025-05-17 22:05:50 +0900

    commit

---
+++ Assets/ProceduralDungeon/Scripts/TileVariant.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System;
+using Random = System.Random;
+
+[Serializable]
+public class TileVariant
+{
+	[SerializeField] GameObject[] variants = new GameObject[0];
+
+	public GameObject GetRandomTile()
+	{
+		Random random = SharedLevelData.Instance.Rand;
+		int randomIndex = random.Next(0, variants.Length);
+		return variants[randomIndex];
+	}
+}
